b7c2623296e5f86960beefc374433678dc81a563
Haonan Ye <haonan.ye@edu.sait.ca>
2024-06-14 23:24:38 -0600

    create folders and files for the program

---
+++ appcess_dev/ViewModels/AppSetPropertyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appcess_dev.ViewModels
+{
+    internal class AppSetPropertyViewModel
+    {
+    }
+}
